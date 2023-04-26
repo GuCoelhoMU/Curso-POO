d59a241cd8bc27b1455e2fc7988c1b18bdddccb4
Gustavo Coelho Garcia <gcoelho.garcia1@gmail.com>
2023-04-26 17:23:09 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,42 @@
+package exercicio;
+
+public abstract class Pessoa {
+	protected String nome;
+	protected int idade;
+	protected int experiencia;
+	
+protected abstract void ganharExp();
+
+public Pessoa(String nome, int idade) {
+	super();
+	this.nome = nome;
+	this.idade = idade;
+	this.experiencia = 0;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public int getIdade() {
+	return idade;
+}
+
+public void setIdade(int idade) {
+	this.idade = idade;
+}
+
+public int getExperiencia() {
+	return experiencia;
+}
+
+public void setExperiencia(int experiencia) {
+	this.experiencia = experiencia;
+}
+
+
+}
